8ba5e1b6545d526f3153fc78022915b3d06ae581
Supriya-1303 <108508381+Padmaja-1303@users.noreply.github.com>
2022-10-17 23:47:36 +0500

    AttendanceList.java

---
+++ School Attendance List/AttendanceList.java
@@ -0,0 +1,29 @@
+package com.school.svv.attendance;
+
+public class AttendanceList 
+{
+	
+		private String full_Name;
+		private String roll_No;
+		
+		public void setFullName(String full_Name)
+		{
+			this.full_Name = full_Name;
+		}
+		
+		public String getFullName()
+		{
+			return this.full_Name;
+		}
+		
+		public void setRoll_No(String roll_No)
+		{
+			this.roll_No = roll_No;
+		}
+		
+		public String getRoll_No()
+		{
+			return this.roll_No;
+		}
+
+}
